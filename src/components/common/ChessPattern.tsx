0d28833261f2fd03404f35ab8c6ac315356314f5
Kieran Emery <kizwiz@hotmail.co.uk>
2025-02-09 19:25:03 +0000

    Make the chess background more prominent.

--- src/components/common/ChessPattern.tsx
+++ src/components/common/ChessPattern.tsx
@@ -2,7 +2,8 @@
 
 export default function ChessPattern() {
   return (
-    <div className="fixed inset-0 z-0 opacity-[0.02] pointer-events-none">
+    <div className="fixed inset-0 z-0 opacity-[0.10] pointer-events-none">
+      {/* Try different opacity values: opacity-[0.05], opacity-[0.08], opacity-[0.1] */}
       <div className="absolute inset-0 grid grid-cols-8 h-screen w-screen">
         {Array.from({ length: 64 }).map((_, i) => {
           const row = Math.floor(i / 8);
